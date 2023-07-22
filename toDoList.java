39c6ed34105fe656b7a9143a7f0566cc1cda846b
Sonu Jana <krishnajana295@gmail.com>
2023-07-23 02:39:04 +0530

    Create toDoList.java

---
+++ toDoList.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class ToDoList implements Serializable {
+    private ArrayList<Task> tasks = new ArrayList<>();
+
+    // Methods for adding, removing, viewing tasks, and saving/loading to/from a file.
+}
